cb84c76e5d7326653e8beb9e9e57eefbd00b4853
bfaber <bfaber@gmail.com>
2018-07-16 13:30:10 -0700

    add set! and fn-set!

--- src/bitsets.clj
+++ src/bitsets.clj
@@ -1,3 +1,17 @@
-(ns bitsets)
+(ns bitsets
+  ;; the bitsets we're working with, java 8. check out these docs:
+  ;; https://docs.oracle.com/javase/8/docs/api/java/util/BitSet.html
+  (import [java.util BitSet]))
 
+(def our-first-bitset!
+  "adorable"
+  (new BitSet))
 
+(defn set!
+  [^BitSet bs ^Integer n]
+  (.set bs n))
+
+(defn functional-set!
+  [^BitSet bs ^Integer n]
+  (.set bs n)
+  bs)
